859e3279de5c1471b330a60f83c49bd8ee1afac6
Raul Torres <rto07@users.noreply.github.com>
2018-05-23 16:17:37 -0500

    Update func.js

--- func.js
+++ func.js
@@ -1,36 +1,12 @@
-/*PROBLEM: Using the shoppingCart variable, create a function that takes the shoppingCart variable and returns the total cost of both items as the total variable.*/
-
-//1st try below
-
 var shoppingCart = [20, 15];
 
-function getTotalCost(prices){
-  let total = 0;
-  // code below
-  var prices =
-      shoppingCart.reduce(function (accumulator, n) {
-        return accumulator + n;
-      });
-  // code above
-  return total + prices;
-}
+function getTotalCost(prices){ 
+  total = 0; 
+  total = prices.reduce((prices, total) => prices + total);
 
-getTotalCost(shoppingCart);
-
-
-/*FEEDBACK: 
-For your code, inside your function definition you are trying to work with the shoppingcart varaible. It's scope is outside the function and defeats the purpose of having a function, and of passing in that value to the function. We typically want to avoid referencing outside variable while inside a function if possible. In this case the given code already passes in a parameter for you to use.*/
-
-//Starting Code
-
-var shoppingCart = [20, 15];
-
-function getTotalCost(prices){
-  let total = 0;
-  // code below
-
-  // code above
   return total;
 }
 
 getTotalCost(shoppingCart);
+
+//some of the confusion came when calling the functionin the console. I was inputing "getTotalCost(45,45);" into the console and received an error message. Took me a while to figure out all I had to do was change the values within the shoppingCart variable. Duh. 
